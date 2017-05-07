7a8e877a624568e6def63a6ffb45296f454a3c71
Frederik Spang <frederik@frederikspang.dk>
2017-05-07 17:13:16 +0200

    Try new 'or' for default scoping not working with OR directly in the query

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -16,7 +16,7 @@ module Paranoia
     end
 
     def only_deleted
-      with_deleted.where.not("#{paranoia_column} >= NOW()")
+      with_deleted.where("#{paranoia_column} < NOW()")
     end
     alias deleted only_deleted
 
@@ -215,7 +215,7 @@ end
 ActiveSupport.on_load(:active_record) do
   class ActiveRecord::Base
     def self.paranoia_scope
-      where("#{paranoia_column} IS NULL OR #{paranoia_column} >= NOW()")
+      where("#{paranoia_column} IS NULL").or(where("{paranoia_column} >= NOW()"))
     end
 
     def self.acts_as_paranoid(options = {})
